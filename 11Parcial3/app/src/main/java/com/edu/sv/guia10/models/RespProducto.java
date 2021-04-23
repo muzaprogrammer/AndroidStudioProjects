fae744bc9c26a256d38b66cd97331e1efd8f6f23
Mario Hdz. Vsqz <brosninja@gmail.com>
2021-04-23 14:58:32 -0600

    parcial

---
+++ 11Parcial3/app/src/main/java/com/edu/sv/guia10/models/RespProducto.java
@@ -0,0 +1,14 @@
+package com.edu.sv.guia10.models;
+
+public class RespProducto {
+    private boolean ok;
+    private Producto resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public Producto getResultado() {
+        return resultado;
+    }
+}
---
+++ 11Parcial3/app/src/main/java/com/edu/sv/guia10/models/Respuesta.java
@@ -0,0 +1,14 @@
+package com.edu.sv.guia10.models;
+
+public class Respuesta {
+    private boolean ok;
+    private int resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
